1b9b691d7e3b495e7dc4992c9c74b82a3885ac98
Volchara <alena_kononova@internet.ru>
2025-04-14 02:43:14 +0300

    dev all show

--- src/projects/ShovPage.tsx
+++ src/projects/ShovPage.tsx
@@ -1,5 +1,9 @@
 import Header from "../Header.tsx";
 import arrow from "../assets/icon_and_logo/arrow_dark.svg"
+import pic1 from "../assets/shov-shov-page/shov_pic1.jpg"
+import pic2 from "../assets/shov-shov-page/shov_pic2.jpg"
+import pic3 from "../assets/shov-shov-page/shov_pic3.jpg"
+import pic4 from "../assets/shov-shov-page/shov_pic4.jpg"
 import {useNavigate} from "react-router";
 
 function ShovPage() {
@@ -18,7 +22,7 @@ function ShovPage() {
     const rowStyle = {
         padding: '0 12px'
     }
-    return(
+    return (
         <div>
             <div style={rowStyle}>
                 <Header/>
@@ -89,6 +93,110 @@ function ShovPage() {
                         <br/>
                         <p className='fs15 color-gray'>ИНСТРУМЕНТЫ</p>
                         <p className='fs20'>Figma</p>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <div style={rowStyle}>
+                <img src={pic1} alt='pic1' style={{minWidth: '100%', marginLeft: '-20px'}}/>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <div style={rowStyle}>
+                <div className='d-flex justify-content-between'>
+                    <p className='fs42Medium' style={{minWidth: '350px'}}>Дизайн процесс</p>
+                    <p className='fs20' style={{maxWidth: '450px'}}>
+                        Дизайн-процесс Шов+Шов включал в себя исследование,
+                        структурно-функциональный анализ, разработку вайрфреймов и создание дизайн-концепции. Итоговый
+                        этап – UI-кит, создание мобильной версии сайта.
+                    </p>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <img src={pic2} alt='pic2' style={{minWidth: '100%', marginLeft: '-5px'}}/>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <div style={rowStyle}>
+                <img src={pic3} alt='pic3' style={{minWidth: '100%', marginLeft: '-20px'}}/>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <div style={rowStyle}>
+                <img src={pic4} alt='pic4' style={{minWidth: '100%', marginLeft: '-20px'}}/>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <div style={rowStyle}>
+                <div className='d-flex justify-content-between' style={{maxWidth: '1300px'}}>
+                    <p className='fs20 bordered'>Участники</p>
+                    <div style={{maxWidth: '400px'}}>
+                        <p className='fs15 color-gray'>РУКОВОДИТЕЛЬ ПРОЕКТА</p>
+                        <p className='fs20'>Алёна Кононова — общая координация
+                            и контроль исполнения.</p>
+                        <br/>
+                        <br/>
+                        <p className='fs15 color-gray'>ВИЗУАЛИЗАТОР</p>
+                        <p className='fs20'>Ли Лолита — разработка визуальной концепции и интерфейсов.</p>
+                    </div>
+                    <div style={{maxWidth: '400px'}}>
+                        <p className='fs15 color-gray'>АРХИВАТОРЫ</p>
+                        <p className='fs20'>Анастасия Визженкова и Яна Васильева — сбор и подготовка материалов.</p>
+                        <br/>
+                        <br/>
+                        <p className='fs15 color-gray'>ИНТЕЛЛЕКТУАЛ</p>
+                        <p className='fs20'>
+                            Ян Кузнецов — исследование аудитории
+                            и проектирование структуры сайта.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <div style={rowStyle}>
+                <p className='fs32'>Смотрите также</p>
+                <br/>
+                <br/>
+                <div className='d-flex justify-content-around' 
+                     style={{minWidth: '1000px', marginLeft: '-50px', cursor:'pointer'}}>
+                    <div style={{maxWidth: '500px'}}
+                         onClick={() => {
+                             navigate("/portfolio/project/baza")
+                         }}>
+                        <img src={require('../assets/index-page/BazaH.jpg')} alt='bazaH'
+                             style={{maxWidth: '550px', borderRadius: '20px'}}/>
+                        <br/>
+                        <br/>
+                        <p className='fs24Medium'>''БАЗА''</p>
+                        <p className='fs20'>
+                            Портфолио компании, которая занимается созданием
+                            дизайна интерьера
+                        </p>
+                    </div>
+                    <div style={{maxWidth: '500px', cursor:'pointer'}}
+                         onClick={() => {
+                             navigate("/portfolio/project/site")
+                         }}>
+                        <img src={require('../assets/index-page/NoizeH.jpg')} alt='noize'
+                             style={{maxWidth: '550px', borderRadius: '20px'}}/>
+                        <br/>
+                        <br/>
+                        <p className='fs24Medium'>Сайт-визитка</p>
+                        <p className='fs20'>
+                            Данный сайт посвящен исполнителю Noize MC
+                        </p>
                     </div>
                 </div>
             </div>
